c8238cc8b5b01a7b3eeb51901f5e8b7333375e99
kanna24060 <134518662+kanna24060@users.noreply.github.com>
2023-08-02 20:31:12 +0530

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@ const awsmobile = {
   aws_cognito_identity_pool_id: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
   aws_cognito_region: 'us-east-1',
   aws_user_pools_id: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
-  aws_user_pools_web_client_id: '6d48ta9aqo5nc5p8bi6lklf86',
+  aws_user_pools_web_client_id: '',
   oauth: {},
   aws_appsync_graphqlEndpoint: 'your-appsync-graphql-endpoint',
   aws_appsync_region: 'us-east-1',
