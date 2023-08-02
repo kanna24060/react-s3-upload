702fb42ead7257b60587023682a950c676cab05c
kanna24060 <134518662+kanna24060@users.noreply.github.com>
2023-08-02 20:11:55 +0530

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,15 @@
+const awsmobile = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_identity_pool_id: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
+  aws_user_pools_web_client_id: '6d48ta9aqo5nc5p8bi6lklf86',
+  oauth: {},
+  aws_appsync_graphqlEndpoint: 'your-appsync-graphql-endpoint',
+  aws_appsync_region: 'us-east-1',
+  aws_appsync_authenticationType: 'API_KEY', // or 'AMAZON_COGNITO_USER_POOLS'
+  aws_s3_bucket: 'lmpq5s3amplifytest143129-test',
+  aws_s3_region: 'us-east-1',
+};
+
+export default awsmobile;
