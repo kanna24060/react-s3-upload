5e0886bee0f1b2c69763eeee1a9dfc7aed6da289
kanna24060 <134518662+kanna24060@users.noreply.github.com>
2023-08-02 19:53:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,82 +1,59 @@
-import { LinkContainer } from 'react-router-bootstrap';
-import React, { Component, Fragment } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import Routes from './Routes';
-import { Auth } from 'aws-amplify';
-
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
+// Import Amplify and Storage
+import Amplify, { Storage } from 'aws-amplify';
+// withAuthenticator is a higher order component that wraps the application with a login page
+import { withAuthenticator } from '@aws-amplify/ui-react';
+// Import the project config files and configure them with Amplify
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
-		this.state = {
-			isAuthenticated: false,
-			isAuthenticating: true
-		};
-	}
+const App = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-	async componentDidMount() {
-		try {
-			if (await Auth.currentSession()) {
-				this.userHasAuthenticated(true);
-			}
-		} catch (e) {
-			if (e !== 'No current user') {
-				alert(e);
-			}
-		}
+  const downloadUrl = async () => {
+    // Creates download url that expires in 5 minutes/ 300 seconds
+    const downloadUrl = await Storage.get('picture.jpg', { expires: 300 });
+    window.location.href = downloadUrl
+  }
 
-		this.setState({ isAuthenticating: false });
-	}
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    try {
+      setLoading(true);
+      // Upload the file to s3 with private access level. 
+      await Storage.put('picture.jpg', file, {
+        level: 'private',
+        contentType: 'image/jpg'
+      });
+      // Retrieve the uploaded file to display
+      const url = await Storage.get('picture.jpg', { level: 'private' })
+      setImageUrl(url);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-	userHasAuthenticated = authenticated => {
-		this.setState({ isAuthenticated: authenticated });
-	};
-
-	handleLogout = async event => {
-		await Auth.signOut();
-
-		this.userHasAuthenticated(false);
-		this.props.history.push('/login');
-	};
-	render() {
-		const childProps = {
-			isAuthenticated: this.state.isAuthenticated,
-			userHasAuthenticated: this.userHasAuthenticated
-		};
-		return (
-			<div className="App container">
-				<Navbar fluid collapseOnSelect>
-					<Navbar.Header>
-						<Navbar.Brand>
-							<Link to="/">Test application</Link>
-						</Navbar.Brand>
-						<Navbar.Toggle />
-					</Navbar.Header>
-					<Navbar.Collapse>
-						<Nav pullRight>
-							{this.state.isAuthenticated ? (
-								<NavItem onClick={this.handleLogout}>Logout</NavItem>
-							) : (
-								<Fragment>
-									<LinkContainer to="/signup">
-										<NavItem>Signup</NavItem>
-									</LinkContainer>
-									<LinkContainer to="/login">
-										<NavItem>Login</NavItem>
-									</LinkContainer>
-								</Fragment>
-							)}
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-				<Routes childProps={childProps} />
-			</div>
-		);
-	}
+  return (
+    <div className="App">
+      <h1> Upload an Image </h1>
+      {loading ? <h3>Uploading...</h3> : <input
+        type="file" accept='image/jpg'
+        onChange={(evt) => handleChange(evt)}
+      />}
+      <div>
+        {imageUrl ? <img style={{ width: "30rem" }} src={imageUrl} /> : <span />}
+      </div>
+      <div>
+        <h2>Download URL?</h2>
+        <button onClick={() => downloadUrl()}>Click Here!</button>
+      </div>
+    </div>
+  );
 }
 
-export default withRouter(App);
+// withAuthenticator wraps your App with a Login component
+export default withAuthenticator(App);
