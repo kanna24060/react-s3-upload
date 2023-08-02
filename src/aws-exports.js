bccc2efef15bfc8279096d2af9a4ca7a1f4de014
kanna24060 <134518662+kanna24060@users.noreply.github.com>
2023-08-02 20:50:13 +0530

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,15 +1,24 @@
-const awsmobile = {
-  aws_project_region: 'us-east-1',
-  aws_cognito_identity_pool_id: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
-  aws_cognito_region: 'us-east-1',
-  aws_user_pools_id: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
-  aws_user_pools_web_client_id: '',
-  oauth: {},
-  aws_appsync_graphqlEndpoint: 'your-appsync-graphql-endpoint',
-  aws_appsync_region: 'us-east-1',
-  aws_appsync_authenticationType: 'API_KEY', // or 'AMAZON_COGNITO_USER_POOLS'
-  aws_s3_bucket: 'lmpq5s3amplifytest143129-test',
-  aws_s3_region: 'us-east-1',
+// export.js
+const amplifyConfig = {
+  // Add the configuration object from aws-exports.js here
+  // For example, if your aws-exports.js looks like this:
+  const awsConfig = {
+    Auth: {
+       region: 'us-east-1',
+       userPoolId: 'us-east-1:17c82090-a71b-4f14-ab15-adcbbec4c010',
+       userPoolWebClientId: '6d48ta9aqo5nc5p8bi6lklf86',
+     },
+     Storage: {
+       bucket: 'lmpq5s3amplifytest143129-test',
+       region: 'us-east-1',
+     }
+  //   // Other service configurations
+  //   // ...
+  // };
+  // You can copy it to amplifyConfig in export.js as follows:
+  // ... copy the configuration object here ...
+
+  // Remember to replace the placeholders with your actual configuration values
 };
 
-export default awsmobile;
+module.exports = amplifyConfig;
