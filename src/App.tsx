a7ffb9d44dceaa5158533d7196dc87c0230d4b59
kanna24060 <134518662+kanna24060@users.noreply.github.com>
2023-08-03 12:38:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,18 @@
-import React, { RefObject } from "react";
-import "./App.css";
-import logo from "./tensult-text-logo-latest.svg";
+import React from 'react';
+import './App.css';
+import BulkUploadComponent from './BulkUploadComponent';
 
-import Amplify, { Storage } from "aws-amplify";
-import awsconfig from "./aws-exports";
-import { withAuthenticator, S3Text } from "aws-amplify-react";
-import Button from "@material-ui/core/Button";
-import { LinearProgress } from "@material-ui/core";
-
-Amplify.configure(awsconfig);
-Storage.configure({ level: "private" });
-
-class App extends React.Component {
-  fileInput: RefObject<any>;
-  constructor(props: any) {
-    super(props);
-    this.fileInput = React.createRef();
-  }
-  progressCallback = (progress: any) => {
-    this.setState({ fileProgress: 100 * (progress.loaded / progress.total) });
-  };
-  uploadFile = () => {
-    const file = this.fileInput.current.files[0];
-    const name = file.name;
-
-    Storage.put(name, file, { progressCallback: this.progressCallback }).then(
-      () => {
-        this.setState({ file: name });
-      }
-    );
-  };
-
-  render() {
-    const fileProgress = this.state ? (this.state as any).fileProgress : 0;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App">
-            
-          <img src={logo} className="App-logo" alt="Tensult" />
-            <p> Pick a file</p>
-            <input type="file" ref={this.fileInput} />
-            <S3Text level="private" path="" />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={this.uploadFile}
-            >
-              Upload
-            </Button>
-            <br /> <br />
-            {fileProgress > 0 && 
-              <LinearProgress variant="determinate" value={fileProgress} />
-            }
-          </div>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>File Upload App</h1>
+      </header>
+      <main>
+        <BulkUploadComponent />
+      </main>
+    </div>
+  );
 }
 
-export default withAuthenticator(App, true);
+export default App;
